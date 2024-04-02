8e806548e21b56c14da4ecdc24049a6601d187bb
Mungai <mungaij697@gmail.com>
2024-04-02 09:25:35 +0300

    complete arrow functions

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function(a,b) {
+    return a/b;
+}
+console.log(divide(2000,100));
+const add = (a,b) => {
+    return a+b;
+}
+console.log(add(2000,100));
+const square = (a) => {
+    return a*a;
+}
+console.log(square(a))
